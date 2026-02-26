3de31ac480df6a9a621f7fe4d4d6989a1f92b3ac
xhos <60789741+xhos@users.noreply.github.com>
2026-02-26 12:50:09 -0500

    detect a window close event

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,16 @@
 
 #include "globals.hpp"
 
+static void onCloseWindow(void* self, SCallbackInfo& info, std::any data) {
+    auto window = std::any_cast<PHLWINDOW>(data);
+
+    // just in case hyprland explodes
+    if (!window)
+        return;
+
+    HyprlandAPI::addNotification(PHANDLE, "[hyprfx] window closed", CHyprColor{1.0, 0.2, 0.2, 1.0}, 5000);
+}
+
 APICALL EXPORT std::string PLUGIN_API_VERSION() {
     return HYPRLAND_API_VERSION;
 }
@@ -15,13 +25,15 @@ APICALL EXPORT PLUGIN_DESCRIPTION_INFO PLUGIN_INIT(HANDLE handle) {
     const std::string CLIENT_HASH = __hyprland_api_get_client_hash();
 
     if (HASH != CLIENT_HASH) {
-        HyprlandAPI::addNotification(PHANDLE, "[hyprfx] Version mismatch (headers != running hyprland)", CHyprColor{1.0, 0.2, 0.2, 1.0}, 5000);
-        throw std::runtime_error("[hyprfx] Version mismatch");
+        HyprlandAPI::addNotification(PHANDLE, "[hyprfx] version mismatch (headers != running hyprland)", CHyprColor{1.0, 0.2, 0.2, 1.0}, 5000);
+        throw std::runtime_error("[hyprfx] version mismatch");
     }
 
-    HyprlandAPI::addNotification(PHANDLE, "[hyprfx] Initialized successfully!", CHyprColor{0.2, 1.0, 0.2, 1.0}, 5000);
+    static auto P = HyprlandAPI::registerCallbackDynamic(PHANDLE, "closeWindow", onCloseWindow);
 
-    return {"hyprfx", "Shader-driven window animations for Hyprland", "xhos", "0.1"};
+    HyprlandAPI::addNotification(PHANDLE, "[hyprfx] init", CHyprColor{0.2, 1.0, 0.2, 1.0}, 5000);
+
+    return {"hyprfx", "sick shader animations or smthn", "xhos", "0.1"};
 }
 
 APICALL EXPORT void PLUGIN_EXIT() {}
